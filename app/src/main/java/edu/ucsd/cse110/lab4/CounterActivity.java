4441d0231eac039ad41ceabc3dbf63efd28fbb74
doublealiu <46761322+doublealiu@users.noreply.github.com>
2023-01-30 15:12:54 -0800

    added counter activity efwubdsgs8ydp8i

--- app/src/main/java/edu/ucsd/cse110/lab4/CounterActivity.java
+++ app/src/main/java/edu/ucsd/cse110/lab4/CounterActivity.java
@@ -3,12 +3,25 @@ package edu.ucsd.cse110.lab4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class CounterActivity extends AppCompatActivity {
+    private int maxCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_counter);
+
+        Bundle extras = getIntent().getExtras();
+        this.maxCount = extras.getInt("max_count");
+
+        TextView counter = findViewById(R.id.counter_view);
+        counter.setText(String.valueOf(maxCount));
+    }
+
+    public void onGoBackClicked(View view) {
+        finish();
     }
 }
