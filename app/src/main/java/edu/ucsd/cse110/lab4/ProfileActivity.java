2a4cf2902d7fd14272cfd9b7cd8e8b307ef2610b
doublealiu <46761322+doublealiu@users.noreply.github.com>
2023-01-30 14:33:51 -0800

    added load and save profile methods

--- app/src/main/java/edu/ucsd/cse110/lab4/ProfileActivity.java
+++ app/src/main/java/edu/ucsd/cse110/lab4/ProfileActivity.java
@@ -2,6 +2,7 @@ package edu.ucsd.cse110.lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
@@ -11,9 +12,36 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        loadProfile();
     }
 
     public void onGoBackClicked(View view) {
         finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        saveProfile();
+    }
+
+    public void loadProfile() {
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        String name = preferences.getString("name", "");
+        String status = preferences.getString("status", "");
+    }
+
+    public void saveProfile() {
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        String name = "TODO";
+        String status = "TODO";
+
+        editor.putString("name", name);
+        editor.putString("status", status);
+
+        editor.apply();
     }
 }
