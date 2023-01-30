ce20538e9e523ba8814c2167a07e3cf9a6818e2e
Arjun Ghoshal <arjunp.ghoshal@gmail.com>
2023-01-30 14:22:55 -0800

    added button methods

--- app/src/main/java/edu/ucsd/cse110/lab4/ProfileActivity.java
+++ app/src/main/java/edu/ucsd/cse110/lab4/ProfileActivity.java
@@ -3,6 +3,7 @@ package edu.ucsd.cse110.lab4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    public void onGoBackClicked(View view) {
+        finish();
     }
 }
